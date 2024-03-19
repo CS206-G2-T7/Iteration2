0b46c6d795dcb74bae19b7d832639ad471cf0c4e
Oi Jun Jie <junjie.oi.2022@scis.smu.edu.sg>
2024-03-20 01:46:18 +0800

    <feat>Database Access Base Code

--- app/src/main/java/com/cs206/cs206_g2t7fe/CreateEvent.java
+++ app/src/main/java/com/cs206/cs206_g2t7fe/CreateEvent.java
@@ -1,12 +1,24 @@
 package com.cs206.cs206_g2t7fe;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
-import android.widget.EditText;
-import android.widget.ImageButton;
+import java.util.Random;
+
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.*;
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.AppCompatButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,22 +31,188 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.cs206.cs206_g2t7fe.databinding.ActivityCreateEventBinding;
 
+import java.util.Calendar;
+import java.util.Random;
+
 public class CreateEvent extends AppCompatActivity {
 
     AppCompatButton submitButton;
 
+    private EditText Event_Name;
+
+    private Button pickDateBtn;
+    private TextView selectedDateTV, Event_Date;
+
+
     private AppBarConfiguration appBarConfiguration;
 //    private ActivityCreateEventBinding binding;
+
+    // creating a variable for our
+    // Firebase Database.
+    FirebaseDatabase firebaseDatabase;
+
+    // creating a variable for our Database
+    // Reference for Firebase.
+    DatabaseReference databaseReference;
+
+    // creating a variable for
+    // our object class
+    UserEvents userEvents;
+
+    private Calendar selectedDate = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
-        submitButton = (AppCompatButton) findViewById(R.id.btn_create_event);
-        submitButton.setOnClickListener(new View.OnClickListener(){
+        String date_out;
+
+        // initializing our edittext and button
+        Event_Name = findViewById(R.id.event_name);
+        Event_Date = selectedDateTV;
+        // below line is used to get the
+        // instance of our FIrebase database.
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        // below line is used to get reference for our database.
+        databaseReference = firebaseDatabase.getReference("UserEvents");
+
+        // initializing our object
+        // class variable.
+        userEvents = new UserEvents();
+
+        // on below line we are initializing our variables.
+        pickDateBtn = findViewById(R.id.idBtnPickDate);
+        selectedDateTV = findViewById(R.id.idTVSelectedDate);
+
+        // on below line we are adding click listener for our pick date button
+        pickDateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addEventLocation();
+                // on below line we are getting
+                // the instance of our calendar.
+                final Calendar c = Calendar.getInstance();
+
+                // on below line we are getting
+                // our day, month and year.
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+
+                // on below line we are creating a variable for date picker dialog.
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        // on below line we are passing context.
+                        CreateEvent.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                selectedDate.set(year, monthOfYear, dayOfMonth);
+                                // on below line we are setting date to our text view.
+                                selectedDateTV.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                            }
+                        },
+                        // on below line we are passing year,
+                        // month and day for selected date in our date picker.
+                        year, month, day);
+                // at last we are calling show to
+                // display our date picker dialog.
+                datePickerDialog.show();
+            }
+
+        });
+
+        final String[] event_name = new String[1];
+        Event_Name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                event_name[0] = s.toString();
+            }
+        });
+
+
+        submitButton = (AppCompatButton) findViewById(R.id.btn_create_event);
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            // getting text from our edittext fields.
+            String name = Event_Name.getText().toString();
+            String date = selectedDateTV.getText().toString();
+
+            @Override
+            public void onClick(View v) {
+                // below line is for checking whether the
+                // edittext fields are empty or not.
+                System.out.println("Name: " + name);
+                System.out.println(selectedDate);
+                long epochMillis = selectedDate.getTimeInMillis();
+                if (epochMillis == 0){
+                    Toast.makeText(CreateEvent.this, "Please add some date.", Toast.LENGTH_SHORT).show();
+                }
+                else if (TextUtils.isEmpty(event_name[0])) {
+                    // if the text fields are empty
+                    // then show the below message.
+                    Toast.makeText(CreateEvent.this, "Please add the event data.", Toast.LENGTH_SHORT).show();
+                }else {
+                    // else call the method to add
+                    // data to our database.
+                    System.out.println("This is the date: " + epochMillis);
+                    addDataToFirebase("junjieoi", name, epochMillis);
+                    addEventLocation();
+                }
+            }
+        });
+    }
+
+    public String generateRandomString(int length) {
+        char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            char c = chars[random.nextInt(chars.length)];
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
+    private void addDataToFirebase(String username, String eventName, Long dateIn) {
+
+        String randomString = generateRandomString(10); // Generates a random string of 10 characters
+        System.out.println(randomString);
+
+        userEvents.setEventID(randomString);
+        userEvents.setEventName(eventName);
+        userEvents.setEventDate(dateIn);
+        userEvents.setUserID(username);
+
+        System.out.println(userEvents.getEventID());
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // inside the method of on Data change we are setting
+                // our object class to our database reference.
+                // data base reference will sends data to firebase.
+                databaseReference.setValue(userEvents);
+
+                // after adding this data we are showing toast message.
+                Toast.makeText(CreateEvent.this, "data added", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // if the data is not added or it is cancelled then
+                // we are displaying a failure toast message.
+                Toast.makeText(CreateEvent.this, "Fail to add data " + error, Toast.LENGTH_SHORT).show();
             }
         });
     }
