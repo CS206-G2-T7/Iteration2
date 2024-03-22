ef7102dfcb3f1893438dd465e03d93f88c780258
Oi Jun Jie <junjie.oi.2022@scis.smu.edu.sg>
2024-03-22 16:48:59 +0800

    <feat> Completed Full Feature Of Event Details Page
    Managed to hide the nav_host_fragment so that it will not show the "This is Home Fragment" message

--- app/src/main/java/com/cs206/cs206_g2t7fe/EventDetails.java
+++ app/src/main/java/com/cs206/cs206_g2t7fe/EventDetails.java
@@ -1,6 +1,14 @@
 package com.cs206.cs206_g2t7fe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.fragment.NavHostFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -11,6 +19,8 @@ import com.cs206.cs206_g2t7fe.databinding.ActivityEventDetailsBinding;
 
 public class EventDetails extends AppCompatActivity {
 
+    ImageButton backButton;
+
     private ActivityEventDetailsBinding binding;
 
     @Override
@@ -19,6 +29,18 @@ public class EventDetails extends AppCompatActivity {
 
         binding = ActivityEventDetailsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().hide();
+
+        backButton = (ImageButton) findViewById(R.id.backButtonEventDetails);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backToMainPage();
+            }
+        });
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -29,6 +51,20 @@ public class EventDetails extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_event_details);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        NavHostFragment navHostFragment = (NavHostFragment) fragmentManager.findFragmentById(R.id.nav_host_fragment_activity_event_details);
+        if (navHostFragment != null) {
+            fragmentManager.beginTransaction().hide(navHostFragment).commit();
+            // Use this to show it again
+            // fragmentManager.beginTransaction().show(navHostFragment).commit();
+        }
+    }
+
+
+    public void backToMainPage(){
+        Intent intent = new Intent(this, LandingPage.class);
+        startActivity(intent);
     }
 
 }
