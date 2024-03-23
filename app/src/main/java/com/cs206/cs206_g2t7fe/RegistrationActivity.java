0ec60dc0ac518f6b30648b89c06b482863323a99
Oi Jun Jie <junjie.oi.2022@scis.smu.edu.sg>
2024-03-23 20:29:33 +0800

    <feat>Registration Functionality Base Code

---
+++ app/src/main/java/com/cs206/cs206_g2t7fe/RegistrationActivity.java
@@ -0,0 +1,13 @@
+package com.cs206.cs206_g2t7fe;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
